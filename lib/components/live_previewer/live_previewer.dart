6dd33f3c0bfd167a6cbfd956934457993f7c22f6
git-xiaocao <xiao-cao-x@hotmail.com>
2022-07-23 19:50:16 +0800

    * 修复直播加载更多失败

--- lib/components/live_previewer/live_previewer.dart
+++ lib/components/live_previewer/live_previewer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pixiv_dart_api/model/live.dart';
+import 'package:pixiv_func_mobile/components/avatar_from_url/avatar_from_url.dart';
 import 'package:pixiv_func_mobile/components/image_from_url/image_from_url.dart';
 import 'package:pixiv_func_mobile/pages/live/live.dart';
 import 'package:pixiv_func_mobile/widgets/text/text.dart';
@@ -18,12 +19,65 @@ class LivePreviewer extends StatelessWidget {
           builder: (BuildContext context, BoxConstraints constraints) {
             //固定的
             final previewHeight = constraints.maxWidth * 0.56;
+
+            final Widget widget;
+            if (null == live.thumbnailImageUrl) {
+              widget = Container(
+                width: double.infinity,
+                height: previewHeight,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+                child: const TextWidget('无封面'),
+              );
+            } else {
+              widget = Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(12)),
+                    child: ImageFromUrl(live.thumbnailImageUrl!, width: double.infinity, height: previewHeight, fit: BoxFit.fitWidth),
+                  ),
+                  Positioned(
+                    right: 5,
+                    top: 8,
+                    child: Container(
+                      padding: const EdgeInsets.all(2),
+                      decoration: BoxDecoration(
+                        color:Colors.black.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.supervisor_account, color: Colors.white, size: 16),
+                          SizedBox(width: 5),
+                          TextWidget(live.memberCount.toString(), fontSize: 12, color: Colors.white),
+                          SizedBox(width: 5),
+                          Icon(Icons.remove_red_eye_outlined, color: Colors.white, size: 16),
+                          SizedBox(width: 5),
+                          TextWidget(live.totalAudienceCount.toString(), fontSize: 12, color: Colors.white),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 10,
+                    bottom: 10,
+                    child: Row(
+                      children: [
+                        for (final item in live.performers)
+                          Padding(
+                            padding: const EdgeInsets.only(right: 8),
+                            child: AvatarFromUrl(item.user.profileImageUrls.medium, radius: 30),
+                          ),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            }
             return GestureDetector(
               onTap: () => Get.to(LivePage(id: live.id, name: live.name)),
-              child: ClipRRect(
-                borderRadius:  const BorderRadius.all(Radius.circular(12)),
-                child: ImageFromUrl(live.thumbnailImageUrl, width: double.infinity, height: previewHeight, fit: BoxFit.fitWidth),
-              ),
+              child: widget,
             );
           },
         ),
@@ -46,6 +100,32 @@ class LivePreviewer extends StatelessWidget {
               ),
             ),
             if (live.isEnabledMicInput) const Icon(Icons.mic) else const Icon(Icons.mic_off),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const SizedBox(width: 10),
+            AvatarFromUrl(live.owner.user.profileImageUrls.medium, radius: 30),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextWidget(
+                    live.owner.user.name,
+                    fontSize: 14,
+                    overflow: TextOverflow.ellipsis,
+                    isBold: true,
+                  ),
+                  TextWidget(
+                    live.owner.user.account,
+                    fontSize: 12,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ],
