38045f85c77fba35c50f6ff3bc2e5d30b2133093
git-xiaocao <xiao-cao-x@hotmail.com>
2022-09-24 14:25:13 +0800

    + 混淆规则设置不混淆kotlinx.serialization

--- android/app/src/main/kotlin/moe/xiaocao/pixiv/appwidget/AppWidgetWorker.kt
+++ android/app/src/main/kotlin/moe/xiaocao/pixiv/appwidget/AppWidgetWorker.kt
@@ -29,7 +29,7 @@ class AppWidgetWorker(val context: Context, workerParams: WorkerParameters) : Wo
     companion object {
 
         fun enqueueUniquePeriodic(context: Context) {
-            //15分钟一次
+            //15-20分钟一次
             val worker =
                 PeriodicWorkRequest.Builder(AppWidgetWorker::class.java, 20, TimeUnit.MINUTES, 15, TimeUnit.MINUTES).setConstraints(
                     Constraints.Builder().also {
